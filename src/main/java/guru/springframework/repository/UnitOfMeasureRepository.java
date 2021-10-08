b2fb54121e5cb9828c7043647e300ef3cfb1cda9
SIPDOMAIN\zinbhonehtut <zinbhonehtut@gmail.com>
2021-10-08 23:31:11 +0630

    Feature (repository) : add recipe repository, category repository, unit of measure repository

---
+++ src/main/java/guru/springframework/repository/UnitOfMeasureRepository.java
@@ -0,0 +1,14 @@
+package guru.springframework.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.springframework.entity.UnitOfMeasure;
+
+/**
+* @author ZinBhoneHtut
+* @credit guru.springframework
+* @date Oct 8, 2021
+*/
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+}
