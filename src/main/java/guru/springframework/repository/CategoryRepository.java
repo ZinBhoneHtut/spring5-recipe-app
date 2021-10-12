8523ae45cfdbbb0e91192884fdf1d5677855a7d8
SIPDOMAIN\zinbhonehtut <zinbhonehtut@gmail.com>
2021-10-12 22:25:12 +0630

    Feature (repository) : add find by description query in category repo and unit of measure repo

--- src/main/java/guru/springframework/repository/CategoryRepository.java
+++ src/main/java/guru/springframework/repository/CategoryRepository.java
@@ -1,5 +1,7 @@
 package guru.springframework.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import guru.springframework.entity.Category;
@@ -11,4 +13,5 @@ import guru.springframework.entity.Category;
 */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
+	Optional<Category> findByDescription(String description);
 }
